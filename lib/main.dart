0a0ae6cbcdbd6452230cb457957a6d50161f9a33
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-09-17 01:26:39 +0530

    check token & page navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:listify_nodeapi_mongodb/screens/dashboard_screen.dart';
 import 'package:listify_nodeapi_mongodb/screens/login_screen.dart';
 import 'package:listify_nodeapi_mongodb/screens/signup_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized;
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); 
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  runApp(MyApp(token: prefs.getString('token')!));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String token; 
 
-  // This widget is the root of your application.
+  const MyApp({super.key, required this.token});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -21,7 +26,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SignupScreen(),
+      home: (token != null && !JwtDecoder.isExpired(token!)) 
+          ? DashboardScreen(token: token) 
+          : LoginScreen(), // Assuming you want to redirect to LoginScreen if expired or token is null
     );
   }
 }
