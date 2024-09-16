ae23c74f51714c061f4419f2dcdcbaaa32d301d4
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-09-16 22:06:06 +0530

    app colors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color yellow = Color(0xFFECC761);
+  static const Color black = Color(0x00ff0000);
+  static Color white = Colors.white;
+}
