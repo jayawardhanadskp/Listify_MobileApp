fef90cbbe0eef45a20c1d84fac577571b1a5b73e
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-09-18 13:32:33 +0530

    done

--- lib/services/config.dart
+++ lib/services/config.dart
@@ -1,4 +1,4 @@
-final url = 'http://192.168.1.42:3000/';
+final url = 'http://<IPv4 Address>:3000/';
 final registration = url + "registration";
 final login = url + "login";
 final storeTodo = url + "storeTodo";
